603436f5eca6ec7ea61f3ca1893cdaaccfe46952
Martha Scales <mart1c05@stud.kea.dk>
2022-09-09 10:47:58 +0200

    blitz

--- src/main.js
+++ src/main.js
@@ -1,1 +1,45 @@
 import "./style.css";
+import { animate, stagger, inView, scroll, timeline } from "motion"
+
+scroll (
+    animate(
+        ".zoom", {
+            scale: [0, 2], 
+            
+            
+        }
+    )
+)
+inView (
+    animate(
+        ".overlay",
+        
+         {
+            opacity: [0, 1, 0],
+        }, 
+        {
+            duration: 1.5
+        }, 
+        
+
+        
+    )
+);
+
+
+
+
+
+
+
+// const scrollContainer = document.querySelector("#scroll");
+
+// scrollContainer.addEventListener("wheel", (evt) => {
+//     evt.preventDefault();
+//     scrollContainer.scrollLeft += evt.deltaY;
+// });
+
+// https://alvarotrigo.com/blog/scroll-horizontally-with-mouse-wheel-vanilla-java/
+
+
+
