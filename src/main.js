9d1bec0fdeea1f710c4c3488b01c39f4143f92d5
Martha Scales <mart1c05@stud.kea.dk>
2022-09-09 13:02:49 +0200

    blitz

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion"
 
-scroll (
+scroll  (
     animate(
         ".zoom", {
             scale: [0, 2], 
             
             
         }
-    )
+    ),
+    
 )
-inView (
+
+
+inView (".overlaytrigger", () => { 
     animate(
         ".overlay",
         
@@ -18,13 +21,44 @@ inView (
             opacity: [0, 1, 0],
         }, 
         {
-            duration: 1.5
-        }, 
+            duration: 2,
+            
+        },
+
         
 
         
+    ) 
+    animate(
+        ".exit", 
+        {
+            scaleY: [1, 0],
+            translateY: [0, 300]
+
+        },
+        {
+            duration: 1.5,
+            delay: 3
+        }
+    ),
+    animate(
+        ".picture",
+        {
+            opacity: [0, 1]
+        },
+        {
+            duration: 0.5,
+            delay: 4
+        }
+
     )
-);
+  
+     
+    
+
+}
+)
+
 
 
 
